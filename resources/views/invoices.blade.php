87f69b041241a5d640d11e2e4954ce9caba73c73
Caleb Porzio <calebporzio@gmail.com>
2020-01-20 10:56:24 -0500

    wip

--- resources/views/invoices.blade.php
+++ resources/views/invoices.blade.php
@@ -14,13 +14,19 @@
             <div class="relative rounded overflow-hidden">
                 <table class="bg-gray-100 w-full">
                     @foreach ($invoices as  $invoice)
-                        <tr class="border-b hover:bg-gray-200 last:border-b-0">
+                        <tr class="border-b">
                             <td class="p-4">
-                                <span class="font-medium">{{ $invoice->title }}</span>
-                                @if ($invoice->paid_at) <span class="ml-2 bg-gray-300 font-semibold p-1 rounded text-gray-600 text-sm">Paid</span> @endif
+                                <div>
+                                    <span class="font-medium">{{ $invoice->title }}</span>
+                                    @if ($invoice->paid_at) <span class="ml-2 bg-gray-300 font-semibold p-1 rounded text-gray-600 text-sm">Paid</span> @endif
+                                </div>
                             </td>
-                            <td class="p-4 text-right font-mono font-semibold">{{ money($invoice->amount_in_cents) }}</td>
-                            <td class="p-4 text-right"><a href="#" class="text-blue-600">Download</a></td>
+                            <td class="p-4 text-right font-mono font-semibold">
+                                <span>{{ money($invoice->amount_in_cents) }}</span>
+                            </td>
+                            <td class="p-4 text-right">
+                                <span class="text-blue-600">Download</span>
+                            </td>
                         </tr>
                     @endforeach
                 </table>
